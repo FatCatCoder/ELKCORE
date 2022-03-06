83d9dc6523db49d13eb3581bda0de78cd2401c41
FatCatCoder <christian75armand@gmail.com>
2022-03-06 16:55:25 -0500

    Cleaning + ASP routes

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -20,4 +20,32 @@ public class TestController : ControllerBase
         Log.Information("HELLO FROM THE WORLD WIDE WEB");
         return new JsonResult(200);
     }
+    [HttpGet("sucess")]
+    public async Task<IActionResult> GetGoodTest()
+    {
+        Log.Information("Ypu may proceed");
+        return new JsonResult(new{StatusCode = 200, Message = "Everything is all okay"});
+    }
+    [HttpGet("error")]
+    public async Task<IActionResult> GetBadTest()
+    {
+        Log.Information("Bad Auth, Please Try again");
+        return new JsonResult(403);
+    }
+    [HttpGet("exception")]
+    public async Task<IActionResult> GetExceptionTest()
+    {
+        try
+        {
+            Log.Information("I'm about to crash!");
+            throw new System.Exception("Server Has been hit");
+
+            return new JsonResult(403);
+        }
+        catch (System.Exception ex)
+        {
+            return new JsonResult(new {StatusCode = 500, ExceptionMessage = ex.Message});
+        }
+        
+    }
 }
